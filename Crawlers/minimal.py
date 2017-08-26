e8cd458db1dd679b0fc19ac4b01e2241622edf36
Bruno Silva <brunocosta231@gmail.com>
2017-08-26 20:12:38 -0300

    Melhor visualização

--- Crawlers/minimal.py
+++ Crawlers/minimal.py
@@ -1,2 +1,12 @@
+lista = []
 
+nome = "Bruno"
+idade = 21
 
+nome1 = "Carlos"
+idade1 = 30
+
+lista.append([nome, idade])
+lista.append([nome1, idade1])
+
+print(lista)
--- Teste/digitalocean.py
+++ Teste/digitalocean.py
@@ -5,6 +5,8 @@ import pdb
 
 #[0].text_content()
 
+lista = []
+
 #Faz o Parsing da página
 url = "https://www.digitalocean.com/pricing/#droplet"
 pagina = requests.get(url)
@@ -18,33 +20,22 @@ elementos = root.xpath("/html/body/section/div[2]/section[1]/div[2]/div/div/div"
 
 #Nome do produto/Valor por mês
 for vlr in elementos:
-    vlr_m = str(vlr[0][0][1].text_content())
+    vlr_m = str(vlr[0][0][1].text_content()).strip()
 
     #Valor por Hora
     for vlr in elementos:
-        vlr_hr = str(vlr[0][1].text_content())
+        vlr_hr = str(vlr[0][1].text_content()).strip()
 
         #Memoria - nº PC´s - Sdds - Transferencia
         for info in elementos:
-            memoria = str(info[1][0][0].text_content())
-            pcs = str(info[1][1][0].text_content())
-            sdd = str(info[1][2][0].text_content())
-            band = str(info[1][3][0].text_content())
+            memoria = str(info[1][0][0].text_content()).strip()
+            pcs = str(info[1][1][0].text_content()).strip()
+            sdd = str(info[1][2][0].text_content()).strip()
+            band = str(info[1][3][0].text_content()).strip()
 
-            print("Valor p/ mês:", vlr_m)
-            print("Valor p/ hra:", vlr_hr.strip())
-            print("Memória:", memoria.strip())
-            print("PC´s:", pcs.strip())
-            print("SDD Disc:", sdd.strip())
-            print("Tranferencia:", band.strip())
-            print("  ")
+    else:
+        #Adiciona os valores em sub-listas
+        lista.append([vlr_m, vlr_hr, memoria, pcs, sdd, band])
 
-
-
-
-
-
-
-
-
-
+for x in lista:
+    print(x)
